abcd8f24984329e2233d8bf6a80a2acc6c8860ee
gefeilin <lingefei5658@gmail.com>
2025-09-08 02:54:01 -0400

    first commit

--- gym_sepsis/envs/sepsis_env.py
+++ gym_sepsis/envs/sepsis_env.py
@@ -1,22 +1,19 @@
 import gym
-from gym.utils import seeding
-from tensorflow import keras
 import numpy as np
 import os
-from collections import deque
 import pandas as pd
+from collections import deque
+from tensorflow import keras
 from gym import spaces
 
-STATE_MODEL = "model/sepsis_states.model"
-TERMINATION_MODEL = "model/sepsis_termination.model"
-OUTCOME_MODEL = "model/sepsis_outcome.model"
+STATE_MODEL = "model/sepsis_states.h5"
+TERMINATION_MODEL = "model/sepsis_termination.h5"
+OUTCOME_MODEL = "model/sepsis_outcome.h5"
 STARTING_STATES_VALUES = "model/sepsis_starting_states.npz"
 
 NUM_FEATURES = 48  # 46 + action + state index
 NUM_ACTIONS = 24
-
 EPISODE_MEMORY = 10
-
 
 features = ['ALBUMIN', 'ANION GAP', 'BANDS', 'BICARBONATE',
             'BILIRUBIN', 'BUN', 'CHLORIDE', 'CREATININE', 'DiasBP', 'Glucose',
@@ -28,95 +25,80 @@ features = ['ALBUMIN', 'ANION GAP', 'BANDS', 'BICARBONATE',
             'qsofa_gcs_score', 'qsofa_resprate_score', 'elixhauser_hospital',
             'blood_culture_positive', 'action', 'state_idx']
 
+
 class SepsisEnv(gym.Env):
-    """
-    Built from trained models on top of the MIMIC dataset, this
-    Environment simulates the behavior of the Sepsis patient
-    in response to medical interventions.
-    For details see: https://github.com/akiani/gym-sepsis 
-    """
-    metadata = {'render.modes': ['ansi']}
+    """ Sepsis simulation environment built on MIMIC-trained models. """
+    metadata = {'render_modes': ['ansi']}
 
     def __init__(self, starting_state=None, verbose=False):
+        super().__init__()
         module_path = os.path.dirname(__file__)
         self.verbose = verbose
-        self.state_model = keras.models.load_model(os.path.join(module_path, STATE_MODEL))
-        self.termination_model = keras.models.load_model(os.path.join(module_path, TERMINATION_MODEL))
-        self.outcome_model = keras.models.load_model(os.path.join(module_path, OUTCOME_MODEL))
-        self.starting_states = np.load(os.path.join(module_path, STARTING_STATES_VALUES))['sepsis_starting_states']
-        self.seed()
-        self.action_space = spaces.Discrete(24)
 
-        # use a pixel to represent next state
-        self.observation_space = spaces.Box(low=0, high=NUM_ACTIONS, shape=(NUM_FEATURES-2, 1, 1),
-                                            dtype=np.float32)
+        self.state_model = keras.models.load_model(os.path.join(module_path, STATE_MODEL), compile=False)
+        self.termination_model = keras.models.load_model(os.path.join(module_path, TERMINATION_MODEL), compile=False)
+        self.outcome_model = keras.models.load_model(os.path.join(module_path, OUTCOME_MODEL), compile=False)
+
+        self.starting_states = np.load(os.path.join(module_path, STARTING_STATES_VALUES))['sepsis_starting_states']
+
+        self.action_space = spaces.Discrete(NUM_ACTIONS)
+        self.observation_space = spaces.Box(
+            low=-np.inf, high=np.inf, shape=(NUM_FEATURES - 2,), dtype=np.float32
+        )
+
         self.reset(starting_state=starting_state)
-        return
 
     def step(self, action):
-        # create memory of present
-        self.memory.append(np.append(np.append(self.s.reshape((1, NUM_FEATURES - 2)), action), self.state_idx))
-        if self.verbose:
-            print("running on memory: ", self.memory)
 
-        memory_array = np.expand_dims(self.memory, 0)
-        next_state = self.state_model.predict(memory_array[:, :, :-1])
+        self.memory.append(np.append(np.append(self.s.flatten(), action), self.state_idx))
+        memory_array = np.expand_dims(self.memory, 0).astype(np.float32)
 
-        # overwrite constant variables (these should't change during episode)
-        constants = ['age', 'race_white', 'race_black', 'race_hispanic',
-                     'race_other', 'height', 'weight']
-        for constant in constants:
-            idx = features.index(constant)
-            val = self.state_0[idx]
-            next_state[0, idx] = val
+        next_state = self.state_model.predict(memory_array[:, :, :-1], verbose=0)
 
-        termination = self.termination_model.predict(memory_array)
-        outcome = self.outcome_model.predict(memory_array)
+        constants = ['age', 'race_white', 'race_black', 'race_hispanic', 'race_other', 'height', 'weight']
+        for c in constants:
+            idx = features.index(c)
+            next_state[0, idx] = self.state_0[idx]
 
-        termination_categories = ['continue', 'done']
-        outcome_categories = ['death', 'release']
+        
+        termination = self.termination_model.predict(memory_array, verbose=0)
+        outcome = self.outcome_model.predict(memory_array, verbose=0)
 
-        termination_state = termination_categories[np.argmax(termination)]
-        outcome_state = outcome_categories[np.argmax(outcome)]
+        termination_state = np.argmax(termination)
+        outcome_state = np.argmax(outcome)
 
-        reward = 0
-        done = False
+        reward, terminated, truncated = 0, False, False
+        if termination_state == 1:  # "done"
+            terminated = True
+            reward = -15 if outcome_state == 0 else 15
 
-        if termination_state == 'done':
-            done = True
-            if outcome_state == 'death':
-                reward = -15
-            else:
-                reward = 15
-
-        # keep next state in memory
-        self.s = next_state.reshape(46, 1, 1)
+        
+        self.s = next_state[0].astype(np.float32)[:NUM_FEATURES - 2]
         self.state_idx += 1
         self.rewards.append(reward)
-        self.dones.append(done)
-        return self.s, reward, done, {"prob" : 1}
+        self.dones.append(terminated)
 
-    def reset(self, starting_state=None):
-        self.rewards = []
-        self.dones = []
+        return self.s, reward, terminated, truncated, {"prob": 1.0}
+
+    def reset(self, starting_state=None, *, seed=None, options=None):
+        super().reset(seed=seed)
+        self.rewards, self.dones = [], []
         self.state_idx = 0
-        self.memory = deque([np.zeros(shape=[NUM_FEATURES])] * 10, maxlen=10)
+        self.memory = deque([np.zeros(NUM_FEATURES)] * EPISODE_MEMORY, maxlen=EPISODE_MEMORY)
+
         if starting_state is None:
-            self.s = self.starting_states[np.random.randint(0, len(self.starting_states))][:-1]
+            self.s = self.starting_states[np.random.randint(len(self.starting_states))][:-1]
         else:
             self.s = starting_state
 
-        self.s = self.s.reshape(NUM_FEATURES - 2, 1, 1)
+        self.s = self.s.astype(np.float32).flatten()
         self.state_0 = np.copy(self.s)
 
         if self.verbose:
             print("starting state:", self.s)
-        return self.s
 
-    def seed(self, seed=None):
-        seed = seeding.np_random(seed)
-        return [seed]
+        return self.s, {}
 
     def render(self, mode='ansi'):
-        df = pd.DataFrame(self.memory, columns=features, index=range(0, 10))
+        df = pd.DataFrame(list(self.memory), columns=features)
         print(df)
